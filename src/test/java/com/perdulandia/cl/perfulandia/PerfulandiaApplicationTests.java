8eaf925d8e1b25d5c6e200352857875b9260dd78
sdinamarca <s.dinamarca@duocuc.cl>
2025-05-20 09:23:22 -0400

    Initial commit

---
+++ src/test/java/com/perdulandia/cl/perfulandia/PerfulandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perdulandia.cl.perfulandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
