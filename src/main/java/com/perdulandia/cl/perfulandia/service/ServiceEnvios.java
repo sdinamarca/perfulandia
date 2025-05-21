c47faf861f375cb90600c55e435b9efb95e0d01c
sdinamarca <s.dinamarca@duocuc.cl>
2025-05-21 18:22:08 -0400

    Fix issue Repository

--- src/main/java/com/perdulandia/cl/perfulandia/service/ServiceEnvios.java
+++ src/main/java/com/perdulandia/cl/perfulandia/service/ServiceEnvios.java
@@ -11,22 +11,27 @@ import java.util.List;
 @Service
 @Transactional
 public class ServiceEnvios {
+
     @Autowired
     private EnvioRepository envioRepository;
 
     public List<Envios> findAll(){
+
         return envioRepository.findAll();
     }
 
     public Envios findById(long id){
+
         return envioRepository.findById(idPedido).get();
     }
 
     public Envios save(Envios envios) {
+
         return envioRepository.save(envios);
     }
 
     public void delete(Long id) {
+
         envioRepository.deleteById(idPedidos);
     }
 }
